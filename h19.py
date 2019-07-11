d40d5384a8a1e58345dc22f7c9c300cee9b52c94
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-07-11 12:29:11 +0530

    Create h19.py

---
+++ h19.py
@@ -0,0 +1,15 @@
+i,j = map(int,input().split())
+l = []
+for k in range(i):
+    l.append(input().split())
+m = l[0]
+le = len(m)
+for k in range(1,len(l)):
+    n = 0
+    while(n<le):
+        if(m[n] in l[k]):
+            n = n+1
+        else:
+            m.remove(m[n])
+            le = le - 1
+print(m)        
