9d68f4e2677ca1bda9db4da4480ad9a68d54323f
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-07-15 17:35:39 +0530

    Create h21.py

---
+++ h21.py
@@ -0,0 +1,21 @@
+r,col = map(int,input().split())
+m = []
+c = []
+for i in range(r):
+	m.append(list(map(int,input().split())))
+for i in m:
+	if 0 in i:
+		for j in range(len(i)):
+			if(i[j] == 0):
+				c.append(j)
+			i[j] = 0
+for i in m:
+	for j in c:
+		i[j] = 0
+		break
+for i in m:
+	for j in range(len(i)):
+		i[j] = str(i[j])
+for i in m:
+	print(' '.join(i))
+		
