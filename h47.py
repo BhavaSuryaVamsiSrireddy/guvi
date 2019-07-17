537a4ef90b148f7d8a662141fc24dd5d4b877e67
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-07-17 22:30:20 +0530

    Update h47.py

--- h47.py
+++ h47.py
@@ -1,17 +1,2 @@
-n = input()
-l = list(n)
-l.append('@')
-r = []
-res = []
-s = ''
-for i in range(len(l)):
-	if  l[i] != ' ' and l[i] != '@':
-		s = s + l[i]
-		continue
-	r.append(s)
-	s = ''
-le = len(r)
-for i in r:
-	if i != '':
-		res.append(i)
-print(' '.join(res))
+ip=str(input())
+print(" ".join(ip.split()))
