eba26560923d2fe453defedbf22990281a916bf3
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2019-07-22 18:44:20 +0530

    Create h65.py

---
+++ h65.py
@@ -0,0 +1,15 @@
+n,k = map(str,input().split())
+l = list(map(str,input().split()))
+le = len(l)
+i = 0
+while(i<le):
+	if l[i] == k:
+		l.remove(l[i])
+		le = le - 1
+		i = i - 1
+		continue
+	i = i + 1
+if( len(l) == 0 ):
+	print('empty')
+else:
+	print(' '.join(l))
